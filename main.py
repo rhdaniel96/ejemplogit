6ab0bdbd6bb3361020b0425ffd3a7b848668fd87
rdaniel <danie.reyesh@duocuc.cl>
2026-04-29 16:31:34 -0400

    primera version

---
+++ main.py
@@ -0,0 +1,21 @@
+ciclo = True
+while ciclo:
+    print('''
+          Menu Principal
+          1) Comprar
+          2) Vender
+          3) Salir
+          
+          ''')
+    op = int(input("Seleccione: "))
+    match op:
+        case 1:
+            print("Comprar")
+        case 2:
+            print("Vender")
+        case 3:
+            ciclo = False
+        case _:
+            print("Opcion incorrecta")
+            
+            
